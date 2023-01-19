fa8ac7b3c04de317078f3c7deeea60ff4407e5d9
DJ Sures <49293683+DJSures@users.noreply.github.com>
2023-01-18 23:05:24 -0500

    Add files via upload

---
+++ dump ram CPM/build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET Z88DK_DIR=c:\z88dk\
+SET ZCCCFG=%Z88DK_DIR%lib\config\
+SET PATH=%Z88DK_DIR%bin;%PATH%
+
+echo.
+echo ****************************************************************************
+
+zcc +cpm -subtype=nabu main.c -compiler=sdcc -create-app -o DUMPRAM
+
+:: zcc +cpm -subtype=nabu main.c -compiler=sdcc -create-app -lndos -pragma-need=ansiterminal -pragma-define:ansicolumns=51 -o DUMPRAM
+:: Columns can be 85, 80, 64, 51, 42, 40, 46, 32, 28, 24
+
+echo ****************************************************************************
+
+mkfs.cpm -f naburn "C:\My Documents\NABU Internet Adapter\Store\a.dsk"
+
+cpmcp -f naburn    "C:\My Documents\NABU Internet Adapter\Store\a.dsk" DUMPRAM.COM 0:
+
+pause
---
+++ dump ram CPM/main.c
@@ -0,0 +1,78 @@
+void main();
+
+#define BIN_TYPE BIN_CPM
+#define DISABLE_KEYBOARD_INT
+#define DISABLE_VDP
+
+#include <stdio.h>
+#include "../NABULIB/RetroNET-FileStore.h"
+#include "../NABULIB/NABU-LIB.h"
+
+#define REMOTE_FILENAME_LEN 11
+#define REMOTE_FILENAME "DUMPRAM.BIN"
+
+void main() {
+
+  initNABULib();
+
+  puts("");
+  puts("DumpRam by DJ Sures");
+  puts("-------------------");
+  puts("");
+  puts("This will copy all NABU RAM to the");
+  puts("Internet Adapter Store folder.");
+  puts("The process takes appro 1 minute.");
+  puts("");
+  puts("Would you like to continue? (y/n)");
+
+  while (true) {
+
+    uint8_t c = getchar();
+
+    if (c == 'y' || c == 'Y') {
+
+      break;
+    } else if (c == 'n' || c == 'N') {
+   
+      puts("Returning to CPM...");
+
+      return;
+    }
+  }
+
+
+
+
+
+
+
+
+  printf("- Opening remote file: %s\n", REMOTE_FILENAME);
+
+  uint8_t writeFileHandle = rn_fileOpen(REMOTE_FILENAME_LEN, REMOTE_FILENAME, OPEN_FILE_FLAG_READWRITE, 0xff);
+
+  printf("- File handle: %u\n", writeFileHandle);
+
+  puts("- Emptying file");
+
+  rn_fileHandleEmptyFile(writeFileHandle);
+
+  uint16_t i;
+  #define PSIZE 2048
+  for (i = 0; i < 0xffff - PSIZE; i+=PSIZE) {
+
+      printf("- Writing %u to %u\n", i, i + PSIZE);
+
+      rn_fileHandleAppend(writeFileHandle, 0, PSIZE, (void *)i);
+  }
+
+  if (0xffff - i > 0) {
+
+    printf("- Writing %u to %u\n", i, 0xffff);
+    rn_fileHandleAppend(writeFileHandle, 0, 0xffff - i, (void *)i);
+  }
+
+  rn_fileHandleClose(writeFileHandle);
+
+  puts("Done\n");
+}
