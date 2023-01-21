4facece71d7b8a16c7afa7aa0daaca4863bb327d
DJ Sures <49293683+DJSures@users.noreply.github.com>
2023-01-21 02:47:42 -0500

    Delete vt100.h

--- RetroNET Telnet CPM/vt100.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define VT100_WIDTH 39
-#define VT100_HEIGHT 24
-
-void vt100_init(void (*send_response)(char* str));
-void vt100_putc(uint8_t ch);
-void vt100_puts(const char* str);
-
