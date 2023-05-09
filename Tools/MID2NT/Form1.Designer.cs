6802f08698d974ea7881e60473e4c756bad7ca08
DJ Sures <49293683+DJSures@users.noreply.github.com>
2023-05-09 13:23:37 -0600

    Add files via upload

---
+++ Tools/MID2NT/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MID2NT {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.AllowDrop = true;
+      this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(0, 27);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+      this.listBox1.Size = new System.Drawing.Size(800, 108);
+      this.listBox1.TabIndex = 14;
+      this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+      this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+      // 
+      // label1
+      // 
+      this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.label1.Location = new System.Drawing.Point(0, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(800, 27);
+      this.label1.TabIndex = 16;
+      this.label1.Text = "Converts MIDI file to .NT (nabu tracker) file. RTFM ";
+      // 
+      // button1
+      // 
+      this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+      this.button1.Location = new System.Drawing.Point(0, 135);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(800, 37);
+      this.button1.TabIndex = 15;
+      this.button1.Text = "CONVERT TO .NT";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBox1.Location = new System.Drawing.Point(0, 172);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox1.Size = new System.Drawing.Size(800, 278);
+      this.textBox1.TabIndex = 13;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.listBox1);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBox1;
+  }
+}
+
